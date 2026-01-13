081bca3cbc6b6058f08ef76e684f919d4cf90f09
drinkel <lu.li.ad.sm@gmail.com>
2026-01-13 12:43:12 +0100

    Update runRadarGUI_analyze.py

--- runRadarGUI_analyze.py
+++ runRadarGUI_analyze.py
@@ -273,20 +273,14 @@ class Ui_MainWindow(object):
             TabWidget.show()
             MainWindow.hide()'''
 
-    def main():
-        import sys
-
-        app = QtWidgets.QApplication(sys.argv)
-
-        MainWindow = QtWidgets.QMainWindow()
-        ui = Ui_MainWindow()
-        ui.setupUi(MainWindow)
-        MainWindow.show()
-
-        sys.exit(app.exec())
-
 if __name__ == "__main__":
-    main()
+    import sys
 
+    app = QtWidgets.QApplication(sys.argv)
 
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
 
+    sys.exit(app.exec())
